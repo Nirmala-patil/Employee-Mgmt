b4768139191b5d8718c0327dcc0b826a4f4bbfa5
Git project <patilnirmala428@gmail.com>
2024-02-18 14:34:36 +0530

    updated code

--- src/app/register-employee/register-employee.component.ts
+++ src/app/register-employee/register-employee.component.ts
@@ -1,8 +1,6 @@
 import { Component,OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup,Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, FormGroup,ValidationErrors,Validators } from '@angular/forms';
 import { EmployeeDetailsComponent } from '../employee-details/employee-details.component';
-
-
 
 @Component({
   selector: 'app-register-employee',
@@ -10,27 +8,76 @@ import { EmployeeDetailsComponent } from '../employee-details/employee-details.c
   styleUrls: ['./register-employee.component.css']
 })
 export class RegisterEmployeeComponent implements OnInit {
- registerForm!: FormGroup; // Define employeeForm as a FormGroup
-  //title:'Employee-app' | undefined;
-  submitted = false;
+  validateDOB(control: AbstractControl): ValidationErrors | null {
+    const dob = new Date(control.value);
+    const today = new Date();
+    var age = today.getFullYear() - dob.getFullYear();
+    const monthDiff = today.getMonth() - dob.getMonth();
+    if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < dob.getDate())) {
+      age--;
+    }
 
-  constructor(private formBuilder: FormBuilder) { }
+    if (age < 18) {
+      return { tooYoung: true };
+    }
 
+    return null;
+  }
+
+  validateDateNotInFuture(control: AbstractControl): ValidationErrors | null {
+    const selectedDate = new Date(control.value);
+    const today = new Date();
+
+    if (selectedDate > today) {
+      return { futureDate: true };
+    }
+
+    return null;
+  }
+  
+  salaryRangeValidator(control: AbstractControl) {
+    const salary = parseFloat(control.value);
+
+    if (isNaN(salary) || salary < 10000 || salary > 100000) {
+      return { salaryRange: true };
+    }
+
+    return null;
+  }
+
+  constructor() { }
 
   ngOnInit(): void {
-    this.registerForm = this.formBuilder.group({
-      employeeId: ['', [Validators.required, Validators.pattern('[0-9]{8}')]],
-      dob: ['', Validators.required],
-      gender: ['', Validators.required],
-      contactNumber: ['', [Validators.required, Validators.pattern('[0-9]{10}')]],
-      email: ['', [Validators.required, Validators.email]],
-      address: ['', Validators.required],
-      jobTitle: ['', Validators.required],
-      department: ['', Validators.required],
-      salary: ['', [Validators.required, Validators.min(0)]],
-      hireDate: ['', Validators.required]
-    });
   }
+  
+  registerForm = new FormGroup({
+    employeeId: new FormControl("",[Validators.required]),
+    contact: new FormControl("",[Validators.required,Validators.pattern("[0-9]*"),
+    Validators.minLength(10),Validators.maxLength(10)]),
+    email: new FormControl("",[Validators.required,Validators.email]),
+    gender: new FormControl("",[Validators.required]),
+    jobtitle: new FormControl("",[Validators.required]),
+    dob: new FormControl("",[Validators.required,this.validateDOB]),
+    doj: new FormControl("", [Validators.required,this.validateDateNotInFuture]),
+    salary: new FormControl("", [Validators.required, Validators.pattern(/^-?\d*(\.\d+)?$/), this.salaryRangeValidator]),
+    department: new FormControl("",[Validators.required]),
+    street: new FormControl("",[Validators.required]),
+    city: new FormControl("",[Validators.required]),
+    state: new FormControl("",[Validators.required]),
+    zip: new FormControl("",[Validators.required]),
+    /*job-title: new FormControl(""),
+    contact: new FormControl(""),
+    department: new FormControl(""),
+    salary: new FormControl(""),
+    hire-date: new FormControl(""),
+    dob: new FormControl(""),
+    gender: new FormControl(""),
+    email: new FormControl(""),
+    street: new FormControl(""),
+    city: new FormControl(""),
+    state: new FormControl(""),
+    postal-code: new FormControl(""),*/
+  })
   /*onSubmit() {
     this.submitted = true
 
@@ -43,8 +90,46 @@ export class RegisterEmployeeComponent implements OnInit {
 
   registerSubmitted(){
     console.log(this.registerForm.get('employeeId'));
-    this.submitted=true;
+  }
 
+  get EmployeeId(): FormControl {
+    return this.registerForm.get("employeeId") as FormControl;
+  }
+  get Contact(): FormControl {
+    return this.registerForm.get("contact") as FormControl;
+  }
+  get Email(): FormControl {
+    return this.registerForm.get("email") as FormControl;
+  }
+  get Gender(): FormControl {
+    return this.registerForm.get("gender") as FormControl;
+  }
+  get JobTitle(): FormControl {
+    return this.registerForm.get("jobtitle") as FormControl;
+  }
+  get Dob(): FormControl {
+    return this.registerForm.get("dob") as FormControl;
+  }
+  get Doj(): FormControl {
+    return this.registerForm.get("doj") as FormControl;
+  }
+  get Salary(): FormControl {
+    return this.registerForm.get("salary") as FormControl;
+  }
+  get Dept(): FormControl {
+    return this.registerForm.get("department") as FormControl;
+  }
+  get Street(): FormControl {
+    return this.registerForm.get("street") as FormControl;
+  }
+  get City(): FormControl {
+    return this.registerForm.get("city") as FormControl;
+  }
+  get State(): FormControl {
+    return this.registerForm.get("state") as FormControl;
+  }
+  get Zip(): FormControl {
+    return this.registerForm.get("zip") as FormControl;
   }
 }
 
